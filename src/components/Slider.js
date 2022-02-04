bfa973a9a3540dc515472c4f039728be4e94fad0
holovkevych <holovkevych@gmail.com>
2022-02-04 02:41:45 +0200

    slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,14 +4,32 @@ import slide2 from "../assets/img/nike2.jpg"
 import slide3 from "../assets/img/nike3.jpg"
 
 const img = [
-    <img key={slide1} src={slide1} />,
-    <img key={slide2} src={slide2} />,
-    <img key={slide3} src={slide3} />,
+    <img key={slide1} src={slide1} className="slider--img" />,
+    <img key={slide2} src={slide2} className="slider--img" />,
+    <img key={slide3} src={slide3} className="slider--img" />,
 ]
 
 const Slider = () => {
 	const [activeIndex, setActiveIndex] = useState(0);
 	
+    const handleNext = () => {
+        if (activeIndex !== img.length-1) {
+            setActiveIndex(prev => prev + 1)            
+        } else {
+            setActiveIndex(prev => 0)            
+        }
+        console.log(activeIndex)
+    }
+
+    const handlePrev = () => {
+        if (activeIndex !== 0) {
+            setActiveIndex(prev => prev - 1)            
+        } else {
+            setActiveIndex(prev => img.length-1)            
+        }
+        console.log(activeIndex)
+    }
+
 	useEffect(() => {
 	  const interval = setInterval(() => {
 	    setActiveIndex((current) => {
@@ -19,31 +37,28 @@ const Slider = () => {
         
         return res
     })
-	    }, 5000)
+	    }, 10000)
 	    
 	    return () => clearInterval()
 	}, [])
 	 
-	const prevImgIndex = activeIndex ? activeIndex - 1 : img.length - 1
-	const nextImgIndex = activeIndex === img.length - 1 ? 0 : activeIndex + 1
+	// const prevImgIndex = activeIndex ? activeIndex - 1 : img.length - 1
+	// const nextImgIndex = activeIndex === img.length - 1 ? 0 : activeIndex + 1
 
 
 	return (
-			<div className="slider mt-45">
-        <div className="slider--img slider--img-prev"
-                key={prevImgIndex}>
-            {img[prevImgIndex]}
-        </div>
-        <div className="slider--img"
-                key={activeIndex}>
-            {img[activeIndex]}
-        </div>
-        <div className="slider--img slider--img-next"
-                key={nextImgIndex}>
-            {img[nextImgIndex]}
-        </div>
-    </div>
-		)
+            <div className="slider mt-45">
+                <button className="button--prev" onClick={handlePrev}>
+                    <span>&lt;</span>
+                </button>
+                <div> 
+                    {img[activeIndex]} 
+                </div>
+                <button className="button--next" onClick={handleNext}>
+                    <span>&gt;</span>
+                </button>
+            </div>
+        )
 }
 
 export default Slider
